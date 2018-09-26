c39de61699f22c2a75048c381beb07ddd15a43f7
llisahere <llisahere@yahoo.com>
2018-09-25 23:53:24 -0400

    users able to save to db and pull

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -29,7 +31,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -38,9 +43,15 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+        User::create($request->all());
+
+        return redirect('/admin/users');
+
+
+        //return $request->all();
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -9,9 +9,9 @@
     <h1>Create users</h1>
 
 
-        {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+        {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
 
-
+    {{ csrf_field() }}
         <div class="form-group">
             {!! Form::label('name', 'Name:') !!}
             {!! Form::text('name', null, ['class'=>'form-control'])!!}
@@ -19,8 +19,40 @@
         </div>
 
         <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control'])!!}
+        </div>
+
+         <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+             {!! Form::select('role_id',['' => 'Choose Options'] + $roles, null, ['class'=>'form-control'])!!}
+         </div>
+
+
+        <div class="form-group">
+            {!! Form::label('is_active', 'Status:') !!}
+            {!! Form::select('is_active',array(1 => 'Active', 0=>'Not Active'), 0 , ['class'=>'form-control'])!!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('file', 'Title:') !!}
+            {!! Form::file('file', null, ['class'=>'form-control'])!!}
+        </div>
+
+
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class'=>'form-control'])!!}
+        </div>
+
+
+        <div class="form-group">
             {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
         </div>
 
         {!! Form::close() !!}
+
+ @include('includes.form_error')
+
+
 @stop
